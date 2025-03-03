cf87bf7310c885a7e4fe27ef28a67122978a0154
18MIGUEL2003 <agudelomiguel04@gmail.com>
2025-03-03 08:01:26 -0500

    Se agregaron los operadores de asignacion a la carpeta operadores

---
+++ Modulo1-Introduccion _JavaScript/operadores/asignacion.js
@@ -0,0 +1,79 @@
+/*
+Asignación básica (=):
+
+// Declaramos una variable num y la asignamos el valor 5
+let num = 5;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 5
+
+Asignación con suma (+=):
+
+// Declaramos una variable num y la asignamos el valor 5
+let num = 5;
+
+// Sumamos 3 a num y reasignamos el resultado
+num += 3;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 8
+
+Asignación con resta (-=):
+
+// Declaramos una variable num y la asignamos el valor 5
+let num = 5;
+
+// Restamos 3 a num y reasignamos el resultado
+num -= 3;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 2
+
+Asignación con multiplicación (*=):
+
+// Declaramos una variable num y la asignamos el valor 5
+let num = 5;
+
+// Multiplicamos 3 a num y reasignamos el resultado
+num *= 3;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 15
+
+Asignación con división (/=):
+
+// Declaramos una variable num y la asignamos el valor 15
+let num = 15;
+
+// Dividimos num entre 3 y reasignamos el resultado
+num /= 3;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 5
+
+Asignación con módulo (%=):
+
+// Declaramos una variable num y la asignamos el valor 7
+let num = 7;
+
+// Calculamos el resto de la división de num entre 3 y reasignamos el resultado
+num %= 3;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 1
+
+Asignación con exponenciación (**=):
+
+// Declaramos una variable num y la asignamos el valor 5
+let num = 5;
+
+// Elevamos num a la potencia de 3 y reasignamos el resultado
+num **= 3;
+
+// Imprimimos en consola el valor de num
+console.log(num); // Output: 125
+
+Los operadores de asignación compuestos permiten simplificar y acortar el código al combinar
+operaciones aritméticas con asignaciones. Es importante tener en cuenta que estos operadores
+modifican el valor de la variable a la que se aplican.
+*/
