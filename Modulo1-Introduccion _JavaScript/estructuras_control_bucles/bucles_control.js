ec474a488b73e82b25ad3fc823ec9e7bbfa47a95
18MIGUEL2003 <agudelomiguel04@gmail.com>
2025-03-07 16:26:04 -0500

    Explicación breve y sencilla de bucles y condicionales para avanzar más rapido

---
+++ Modulo1-Introduccion _JavaScript/estructuras_control_bucles/bucles_control.js
@@ -0,0 +1,42 @@
+/*
+‌Las estructuras de control de flujo, son intrucciones que nos permiten evaluar si se
+puede cumplir una condición o no, incluso nos puede ayudar a evaluarla n cantidad de veces.
+
+Condicionales
+‌
+
+Los condicionales nos permiten evaluar si una condición cumple o no con lo que estemos evaluando.
+Su sintaxis es muy sencilla, podemos evaluar si la condición es verdadera o falsa. Incluso añadir
+una condición intermedia en el caso de que no se cumpla la primera condición y se deban evaluar más.
+
+Ciclos, Bucles o Loops
+‌
+
+Se le pueden llamar, ciclos, bucles o loops, en ellos se evalua una condición n veces hasta
+que esta se cumpla. En estos podemos encontrar los for, while, entre otros.‌
+
+for
+‌
+
+Un bucle for se repite como mencione hasta que la condición que se esta evaluando se cumpla.
+
+
+
+while
+‌
+
+Ejecuta una sentencia mientras la condición que se este evaluando sea verdadera.
+
+
+
+Switch
+‌
+
+Permite evaluar una expresión e intenta igual el valor de esa expresión a una etiqueta
+llamada case, que es el caso a evaluar. En el caso de que la condición se cumpla o lo que
+tiene el case, se ejecuta la sentencia que este en ese caso.
+
+
+En el switch se usa una sentencia llamada “break”, esta nos permite salir de la condición
+que se esta evaluando.
+*/
