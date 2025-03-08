86997477df3dff4ba4b275c627a2c7a95eb470a7
18MIGUEL2003 <agudelomiguel04@gmail.com>
2025-03-08 08:13:38 -0500

    Agregando el modulo 2 - Funciones declarativas y expresadas

---
+++ Modulo2-Funciones_Arrays/funciones.js
@@ -0,0 +1,22 @@
+/*
+Funciones Declarativas y Expresivas
+
+Una función expresada podría tener la siguiente forma
+
+//Ejemplo de función expresada
+var miFuncion = function(){
+   //Aquí van los enunciados de lo que hará la función expresada
+}
+Otro ejemplo de función expresada es:
+
+//Ejemplos de función autoejecutada o IIFE por las siglas de su nombre en inglés
+(function miOtraFuncion(){})()
+
+El ejemplo incluido en la pregunta no contiene funciones expresadas. Las funciones incluidas
+en la pregunta son funciones "declaradas".
+
+Una función es "declarada" cuando el enunciado empieza con la palabra clave function
+
+Una función es "expresada" cuando una expresión es una función. Una expresión es una unidad
+que devuelve un valor
+*/
